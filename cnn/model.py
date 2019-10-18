6a82b3f9138f64e3d47a3c52f651b6f8240e8e2c
陈熠豪 <chenyiha17@mails.tsinghua.edu.cn>
2019-10-18 16:34:44 +0800

    implement cnn

--- cnn/model.py
+++ cnn/model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
@@ -5,14 +6,19 @@ FLAGS = tf.app.flags.FLAGS
 
 class Model:
     def __init__(self,
+                 drop_rate=0.5,
                  learning_rate=0.001,
                  learning_rate_decay_factor=0.9995):
-        self.x_ = tf.placeholder(tf.float32, [None, 32, 32, 3])
-        self.y_ = tf.placeholder(tf.int32, [None])
 
-        # TODO:  fill the blank of the arguments
-        self.loss, self.pred, self.acc = self.forward()
-        self.loss_val, self.pred_val, self.acc_val = self.forward()
+        n_input = 32
+        n_input_channel = 3
+
+        self.x_ = tf.placeholder(tf.float32, [None, n_input, n_input, n_input_channel])
+        self.y_ = tf.placeholder(tf.int32, [None])
+        self.drop_rate = drop_rate
+
+        self.loss, self.pred, self.acc = self.forward(True)
+        self.loss_val, self.pred_val, self.acc_val = self.forward(False, reuse=True)
 
         self.learning_rate = tf.Variable(float(learning_rate), trainable=False, dtype=tf.float32)
         self.learning_rate_decay_op = self.learning_rate.assign(self.learning_rate * learning_rate_decay_factor)
@@ -30,20 +36,33 @@ class Model:
     def forward(self, is_train, reuse=None):
     
         with tf.variable_scope("model", reuse=reuse):
-            # TODO: implement input -- Conv -- BN -- ReLU -- Dropout -- MaxPool -- Conv -- BN -- ReLU -- Dropout -- MaxPool -- Linear -- loss
-            #        the 10-class prediction output is named as "logits"
-            # Your Conv Layer
-            # Your BN Layer: use batch_normalization_layer function
-            # Your Relu Layer
-            # Your Dropout Layer: use dropout_layer function
-            # Your MaxPool
-            # Your Conv Layer
-            # Your BN Layer: use batch_normalization_layer function
-            # Your Relu Layer
-            # Your Dropout Layer: use dropout_layer function
-            # Your MaxPool
-            # Your Linear Layer
-            logits = tf.Variable(tf.constant(0.0, shape=[100, 10]))  # deleted this line after you implement above layers
+            '''
+            implement input -- Conv -- BN -- ReLU -- Dropout -- MaxPool -- Conv -- BN -- ReLU -- Dropout -- MaxPool -- Linear -- loss
+                   the 10-class prediction output is named as "logits"
+            '''
+
+            # Conv Layer
+            Conv_0 = tf.layers.conv2d(self.x_, filters=10, kernel_size=5)
+            # BN Layer: use batch_normalization_layer function
+            BN_0 = batch_normalization_layer(Conv_0, is_train=is_train)
+            # Relu Layer
+            Relu_0 = tf.nn.relu(BN_0)
+            # Dropout Layer: use dropout_layer function
+            DO_0 = dropout_layer(Relu_0, self.drop_rate, is_train=is_train)
+            # MaxPool
+            Pool_0 = tf.layers.max_pooling2d(DO_0, pool_size=2, strides=2)
+            # Conv Layer
+            Conv_1 = tf.layers.conv2d(Pool_0, filters=20, kernel_size=4)
+            # BN Layer: use batch_normalization_layer function
+            BN_1 = batch_normalization_layer(Conv_1, is_train=is_train)
+            # Relu Layer
+            Relu_1 = tf.nn.relu(BN_1)
+            # Dropout Layer: use dropout_layer function
+            DO_1 = dropout_layer(Relu_1, self.drop_rate, is_train=is_train)
+            # MaxPool
+            Pool_1 = tf.layers.max_pooling2d(DO_1, pool_size=2, strides=2)
+            # Linear Layer
+            logits = tf.layers.dense(tf.layers.flatten(Pool_1), 10)
 
         loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y_, logits=logits))
         pred = tf.argmax(logits, 1)  # Calculate the prediction result
@@ -53,14 +72,26 @@ class Model:
         return loss, pred, acc
 
 def batch_normalization_layer(incoming, is_train=True):
-    # TODO: implement the batch normalization function and applied it on fully-connected layers
-    # NOTE:  If isTrain is True, you should use mu and sigma calculated based on mini-batch
-    #       If isTrain is False, you must use mu and sigma estimated from training data
-    pass
+    '''
+    implement the batch normalization function and applied it on fully-connected layers
+    NOTE:  If isTrain is True, you should use mu and sigma calculated based on mini-batch
+          If isTrain is False, you must use mu and sigma estimated from training data
+    '''
+    outgoing = tf.layers.batch_normalization(incoming, training=is_train, momentum=0.9)
+    return outgoing
     
-def dropout_layer(incoming, drop_rate, is_train=True):
-    # TODO: implement the dropout function and applied it on fully-connected layers
-    # Note: When drop_rate=0, it means drop no values
-    #       If isTrain is True, you should randomly drop some values, and scale the others by 1 / (1 - drop_rate)
-    #       If isTrain is False, remain all values not changed
-    pass
+def dropout_layer(incoming, drop_rate, is_train=True, alternative=False):
+    '''
+    implement the dropout function and applied it on fully-connected layers
+    NOTE: When drop_rate=0, it means drop no values
+          If isTrain is True, you should randomly drop some values, and scale the others by 1 / (1 - drop_rate)
+          If isTrain is False, remain all values not changed
+    '''
+    if is_train:
+        if alternative:
+            outgoing = tf.scalar_mul(1/(1-drop_rate), incoming)
+        else:
+            outgoing = tf.nn.dropout(incoming, rate=drop_rate)
+    else:
+        outgoing = incoming
+    return outgoing
