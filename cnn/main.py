fix batch normalization

cnn/main.py
@@ -9,7 +9,7 @@ from model import Model
 from load_data import load_cifar_4d
 
 tf.app.flags.DEFINE_integer("batch_size", 100, "batch size for training")
-tf.app.flags.DEFINE_integer("num_epochs", 20, "number of epochs")
+tf.app.flags.DEFINE_integer("num_epochs", 500, "number of epochs")
 tf.app.flags.DEFINE_float("drop_rate", 0.5, "drop out rate")
 tf.app.flags.DEFINE_boolean("is_train", True, "False to inference")
 tf.app.flags.DEFINE_string("data_dir", "../cifar-10_data", "data dir")
@@ -80,7 +80,7 @@ with tf.Session() as sess:
         X_train, X_test, y_train, y_test = load_cifar_4d(FLAGS.data_dir)
         X_val, y_val = X_train[40000:], y_train[40000:]
         X_train, y_train = X_train[:40000], y_train[:40000]
-        cnn_model = Model()
+        cnn_model = Model(drop_rate=FLAGS.drop_rate)
         if tf.train.get_checkpoint_state(FLAGS.train_dir):
             cnn_model.saver.restore(sess, tf.train.latest_checkpoint(FLAGS.train_dir))
         else:

cnn/model.py
@@ -44,7 +44,7 @@ class Model:
             # Conv Layer
             Conv_0 = tf.layers.conv2d(self.x_, filters=10, kernel_size=5)
             # BN Layer: use batch_normalization_layer function
-            BN_0 = batch_normalization_layer(Conv_0, is_train=is_train)
+            BN_0 = batch_normalization_layer(Conv_0, is_train=True)
             # Relu Layer
             Relu_0 = tf.nn.relu(BN_0)
             # Dropout Layer: use dropout_layer function
@@ -54,7 +54,7 @@ class Model:
             # Conv Layer
             Conv_1 = tf.layers.conv2d(Pool_0, filters=20, kernel_size=4)
             # BN Layer: use batch_normalization_layer function
-            BN_1 = batch_normalization_layer(Conv_1, is_train=is_train)
+            BN_1 = batch_normalization_layer(Conv_1, is_train=True)
             # Relu Layer
             Relu_1 = tf.nn.relu(BN_1)
             # Dropout Layer: use dropout_layer function

mlp/model.py
@@ -53,7 +53,7 @@ class Model:
             hidden_1 = tf.add(tf.matmul(self.x_, self.weights["h1"]), self.biases["h1"])
 
             # BN Layer: use batch_normalization_layer function
-            BN_layer = batch_normalization_layer(hidden_1, is_train=is_train)
+            BN_layer = batch_normalization_layer(hidden_1, is_train=True)
 
             # Relu Layer
             Relu_layer = tf.nn.relu(BN_layer)