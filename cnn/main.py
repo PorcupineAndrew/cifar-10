f9746f08eb73d7af6bd5c73fc909a10c21a81729
陈熠豪 <chenyiha17@mails.tsinghua.edu.cn>
2019-10-20 11:46:13 +0800

    add plot

--- cnn/main.py
+++ cnn/main.py
@@ -7,14 +7,17 @@ import os
 import time
 from model import Model
 from load_data import load_cifar_4d
+import matplotlib.pyplot as plt 
 
 tf.app.flags.DEFINE_integer("batch_size", 100, "batch size for training")
-tf.app.flags.DEFINE_integer("num_epochs", 500, "number of epochs")
+tf.app.flags.DEFINE_integer("num_epochs", 100, "number of epochs")
 tf.app.flags.DEFINE_float("drop_rate", 0.5, "drop out rate")
 tf.app.flags.DEFINE_boolean("is_train", True, "False to inference")
 tf.app.flags.DEFINE_string("data_dir", "../cifar-10_data", "data dir")
 tf.app.flags.DEFINE_string("train_dir", "./train", "training dir")
 tf.app.flags.DEFINE_integer("inference_version", 0, "the version for inference")
+tf.app.flags.DEFINE_integer("plot_epoch", 10, "the epoch to plot")
+tf.app.flags.DEFINE_string("plot_dir", "./plot", "plo dir")
 FLAGS = tf.app.flags.FLAGS
 
 
@@ -37,19 +40,49 @@ def shuffle(X, y, shuffle_parts):
     return X, y
 
 
-def train_epoch(model, sess, X, y): # Training Process
-    loss, acc = 0.0, 0.0
-    st, ed, times = 0, FLAGS.batch_size, 0
-    while st < len(X) and ed <= len(X):
-        X_batch, y_batch = X[st:ed], y[st:ed]
-        feed = {model.x_: X_batch, model.y_: y_batch}
-        loss_, acc_, _ = sess.run([model.loss, model.acc, model.train_op], feed)
-        loss += loss_
-        acc += acc_
-        st, ed = ed, ed+FLAGS.batch_size
-        times += 1
-    loss /= times
-    acc /= times
+def train_epoch(model, sess, X, y, plot=False, epoch=0): # Training Process
+    if not plot:
+        loss, acc = 0.0, 0.0
+        st, ed, times = 0, FLAGS.batch_size, 0
+        while st < len(X) and ed <= len(X):
+            X_batch, y_batch = X[st:ed], y[st:ed]
+            feed = {model.x_: X_batch, model.y_: y_batch}
+            loss_, acc_, _= sess.run([model.loss, model.acc, model.train_op], feed)
+            loss += loss_
+            acc += acc_
+            st, ed = ed, ed+FLAGS.batch_size
+            times += 1
+        loss /= times
+        acc /= times
+    else:
+        loss, acc = [], []
+        st, ed = 0, FLAGS.batch_size
+        while st < len(X) and ed <= len(X):
+            X_batch, y_batch = X[st:ed], y[st:ed]
+            feed = {model.x_: X_batch, model.y_: y_batch}
+            loss_, acc_, _= sess.run([model.loss, model.acc, model.train_op], feed)
+            loss.append(loss_)
+            acc.append(acc_)
+            st, ed = ed, ed+FLAGS.batch_size
+        # plot loss
+        x = list([i+1 for i in range(len(loss))])
+        plt.figure(figsize=(8,4))
+        plt.xlabel("batch")
+        plt.ylabel("loss")
+        plt.plot(x, loss, linewidth=1)
+        plt.title(f"epoch_{epoch} loss", fontweight=800)
+        plt.savefig(os.path.join(FLAGS.plot_dir, f"epoch{epoch}_loss.png"))
+        # plot acc
+        plt.figure(figsize=(8,4))
+        plt.xlabel("batch")
+        plt.ylabel("acc")
+        plt.ylim(0, 1.0)
+        plt.plot(x, acc, linewidth=1)
+        plt.title(f"epoch_{epoch} accuracy", fontweight=800)
+        plt.savefig(os.path.join(FLAGS.plot_dir, f"epoch{epoch}_acc.png"))
+
+        loss = np.mean(loss)
+        acc = np.mean(acc)
     return acc, loss
 
 
@@ -73,9 +106,12 @@ def inference(model, sess, X): # Test Process
     return sess.run([model.pred_val], {model.x_: X})[0]
 
 
+
 with tf.Session() as sess:
     if not os.path.exists(FLAGS.train_dir):
         os.mkdir(FLAGS.train_dir)
+    if not os.path.exists(FLAGS.plot_dir):
+        os.mkdir(FLAGS.plot_dir)
     if FLAGS.is_train:
         X_train, X_test, y_train, y_test = load_cifar_4d(FLAGS.data_dir)
         X_val, y_val = X_train[40000:], y_train[40000:]
@@ -90,7 +126,10 @@ with tf.Session() as sess:
         best_val_acc = 0.0
         for epoch in range(FLAGS.num_epochs):
             start_time = time.time()
-            train_acc, train_loss = train_epoch(cnn_model, sess, X_train, y_train)
+            if epoch % FLAGS.plot_epoch == 0:
+                train_acc, train_loss = train_epoch(cnn_model, sess, X_train, y_train, plot=True, epoch=epoch)
+            else:
+                train_acc, train_loss = train_epoch(cnn_model, sess, X_train, y_train, plot=False)
             X_train, y_train = shuffle(X_train, y_train, 1)
 
             val_acc, val_loss = valid_epoch(cnn_model, sess, X_val, y_val)
